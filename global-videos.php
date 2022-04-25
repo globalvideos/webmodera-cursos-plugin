24dab86c955bfb59b435d8d6e7a228ac6138c941
Rinaldo Pavanello <rpavanello@gmail.com>
2022-04-25 10:17:39 -0300

    Update global-videos.php

--- global-videos.php
+++ global-videos.php
@@ -3,7 +3,7 @@
 Plugin Name: Global Videos (CURSOS)
 Plugin URI: https://www.globalvideos.com.br
 description: Plugins para os sites de cursos
-Version: 1.0.7
+Version: 1.0.8
 Author: Global Videos
 Author URI: https://www.globalvideos.com.br
 License: GPL2
